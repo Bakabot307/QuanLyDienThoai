3723ad32f818281cf6b399df645eca483f165149
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2021-12-03 23:11:13 +0700

    update

--- DuAn/src/com/raven/form/SanPhamPanel.java
+++ DuAn/src/com/raven/form/SanPhamPanel.java
@@ -285,10 +285,7 @@ model.addRow(row);
 
         tblSanPham.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8"
